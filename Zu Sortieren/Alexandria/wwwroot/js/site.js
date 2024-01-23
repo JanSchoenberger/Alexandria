fd0ca5db692ca94a9ed8533e2c06f433b67195ad
JanSchoenberger <janschoenberger@outlook.de>
2024-01-23 17:07:51 +0100

    Repository update.

--- Zu Sortieren/Alexandria/wwwroot/js/site.js
+++
@@ -1,5 +0,0 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-// Das ist sehr toll, dass man das hier hinzufügen kann, allgemien wirkt es so, dass asp.net nur mehr Funktionen hinzufügt, dafür ist es aber auch komplexer.
-// Dafür muss ich auf jeden Fall noch weitere Kurse durcharbeiten, es lohnt sich.
-// Write your JavaScript code.
