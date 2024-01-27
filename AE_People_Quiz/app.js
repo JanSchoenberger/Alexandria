6eb6712cafeff8f2f8ec7d8f1840a840f05a5425
JanSchoenberger <janschoenberger@outlook.de>
2024-01-27 18:15:59 +0100

    Fix der Bildanzeige.

--- AE_People_Quiz/app.js
+++ AE_People_Quiz/app.js
@@ -161,8 +161,8 @@ function showSolution() {
 
 async function readJSON() {
 
-
-        const response = await fetch('./kategories/painter.json'); //image_dictionary.json');
+        const response = await fetch('image_dictionary.json');
+       // const response = await fetch('./kategories/painter.json'); //image_dictionary.json');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
